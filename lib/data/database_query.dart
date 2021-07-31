2d03f373130932263a8375ce32c7fc944fa7c715
jmapps <jmappsmuslim@gmail.com>
2021-07-31 14:33:46 +0300

    Added favorite content page

--- lib/data/database_query.dart
+++ lib/data/database_query.dart
@@ -11,10 +11,22 @@ class DatabaseQuery {
     return mainContent!;
   }
 
+  Future<List<ContentItem>> getFavoriteContent() async {
+    var dbClient = await con.db;
+    var res = await dbClient.query('Table_of_content', where: 'favorite == 1');
+    List<ContentItem>? favoriteContent = res.isNotEmpty ? res.map((c) => ContentItem.fromMap(c)).toList() : null;
+    return favoriteContent!;
+  }
+
   Future<List<ContentItem>> getContentSearchResult(String text) async {
     var dbClient = await con.db;
     var res = await dbClient.rawQuery("SELECT * FROM Table_of_content WHERE _id LIKE '%$text%' OR content LIKE '%$text%'");
     List<ContentItem>? searchResult = res.isNotEmpty ? res.map((c) => ContentItem.fromMap(c)).toList() : null;
     return searchResult!;
+  }
+
+  addRemoveFavorite(int state, int _id) async {
+    var dbClient = await con.db;
+    await dbClient.rawQuery('UPDATE Table_of_content SET favorite = $state WHERE _id == $_id');
   }
 }
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gems_tarifi/pages/content_page.dart';
+import 'package:gems_tarifi/pages/favorite_content_page.dart';
 
 class AppRouter {
   Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => ContentPage());
+      case '/favorite':
+        return MaterialPageRoute(builder: (_) => FavoriteContentPage());
       default:
         throw Exception('Invalid route');
     }
