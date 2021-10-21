4848d96a5c170f0eedd212c03ceba1fc81b14765
jmapps <jmappsmuslim@gmail.com>
2021-10-21 09:27:39 +0300

    Small fixes

--- lib/data/database_query.dart
+++ lib/data/database_query.dart
@@ -1,27 +1,27 @@
 import 'package:gems_tarifi/data/database_helper.dart';
-import 'package:gems_tarifi/model/content_item.dart';
+import 'package:gems_tarifi/model/content_model_item.dart';
 
 class DatabaseQuery {
   DatabaseHelper con = DatabaseHelper();
 
-  Future<List<ContentItem>> getAllContent() async {
+  Future<List<ContentModelItem>> getAllContent() async {
     var dbClient = await con.db;
     var res = await dbClient.query('Table_of_content');
-    List<ContentItem>? mainContent = res.isNotEmpty ? res.map((c) => ContentItem.fromMap(c)).toList() : null;
+    List<ContentModelItem>? mainContent = res.isNotEmpty ? res.map((c) => ContentModelItem.fromMap(c)).toList() : null;
     return mainContent!;
   }
 
-  Future<List<ContentItem>> getFavoriteContent() async {
+  Future<List<ContentModelItem>> getFavoriteContent() async {
     var dbClient = await con.db;
     var res = await dbClient.query('Table_of_content', where: 'favorite == 1');
-    List<ContentItem>? favoriteContent = res.isNotEmpty ? res.map((c) => ContentItem.fromMap(c)).toList() : null;
+    List<ContentModelItem>? favoriteContent = res.isNotEmpty ? res.map((c) => ContentModelItem.fromMap(c)).toList() : null;
     return favoriteContent!;
   }
 
-  Future<List<ContentItem>> getContentSearchResult(String text) async {
+  Future<List<ContentModelItem>> getContentSearchResult(String text) async {
     var dbClient = await con.db;
     var res = await dbClient.rawQuery("SELECT * FROM Table_of_content WHERE _id LIKE '%$text%' OR content LIKE '%$text%'");
-    List<ContentItem>? searchResult = res.isNotEmpty ? res.map((c) => ContentItem.fromMap(c)).toList() : null;
+    List<ContentModelItem>? searchResult = res.isNotEmpty ? res.map((c) => ContentModelItem.fromMap(c)).toList() : null;
     return searchResult!;
   }
 
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gems_tarifi/pages/content_page.dart';
-import 'package:gems_tarifi/pages/favorite_content_page.dart';
+import 'package:gems_tarifi/pages/main_page.dart';
 
 class AppRouter {
   Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => ContentPage());
-      case '/favorite':
-        return MaterialPageRoute(builder: (_) => FavoriteContentPage());
+        return MaterialPageRoute(builder: (_) => MainPage(), settings: routeSettings);
       default:
         throw Exception('Invalid route');
     }
