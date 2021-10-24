f7d3af7d7468fc8442b85613b061f2b947f268bf
jmapps <jmappsmuslim@gmail.com>
2021-10-24 14:33:54 +0300

    Small fixes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gems_tarifi/provider/app_settings_state.dart';
 import 'package:gems_tarifi/provider/bottom_navigation_state.dart';
 import 'package:gems_tarifi/provider/item_bookmark_state.dart';
+import 'package:gems_tarifi/provider/scroll_positioned_state.dart';
 import 'package:gems_tarifi/provider/search_state.dart';
 import 'package:gems_tarifi/provider/take_screenshot_state.dart';
 import 'package:gems_tarifi/router/app_router.dart';
@@ -17,12 +18,13 @@ void main() {
         ChangeNotifierProvider(create: (_) => ItemBookmarkState()),
         ChangeNotifierProvider(create: (_) => AppSettingsState()),
         ChangeNotifierProvider(create: (_) => TakeScreenshotState()),
+        ChangeNotifierProvider(create: (_) => ScrollPositionedState()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Жемчужины Тарифи',
-        initialRoute: '/',
         onGenerateRoute: _appRouter.onGenerateRoute,
+        initialRoute: '/',
         theme: ThemeData(fontFamily: 'Gilroy'),
       ),
     ),
