5ae1c3fa6c24974cb0df44c9e6616be19837052f
jmapps <jmappsmuslim@gmail.com>
2022-07-20 22:58:00 +0300

    Changed main and content page

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:gems_tarifi/provider/app_settings_state.dart';
-import 'package:gems_tarifi/provider/bottom_navigation_state.dart';
-import 'package:gems_tarifi/provider/item_bookmark_state.dart';
-import 'package:gems_tarifi/provider/scroll_positioned_state.dart';
-import 'package:gems_tarifi/provider/search_state.dart';
-import 'package:gems_tarifi/provider/take_screenshot_state.dart';
-import 'package:gems_tarifi/router/app_router.dart';
+import 'package:gems_tarifi/presentation/pages/main_page.dart';
+import 'package:gems_tarifi/provider/main_app_state.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  var _appRouter = AppRouter();
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => SearchState()),
-        ChangeNotifierProvider(create: (_) => BottomNavigationState()),
-        ChangeNotifierProvider(create: (_) => ItemBookmarkState()),
-        ChangeNotifierProvider(create: (_) => AppSettingsState()),
-        ChangeNotifierProvider(create: (_) => TakeScreenshotState()),
-        ChangeNotifierProvider(create: (_) => ScrollPositionedState()),
+        ChangeNotifierProvider<MainAppState>(
+          create: (_) => MainAppState(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Жемчужины Тарифи',
-        onGenerateRoute: _appRouter.onGenerateRoute,
-        initialRoute: '/',
         theme: ThemeData(fontFamily: 'Gilroy'),
+        home: MainPage(),
       ),
     ),
   );
---
+++ lib/presentation/pages/content_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gems_tarifi/presentation/widgets/content_list.dart';
+
+class ContentPage extends StatelessWidget {
+  ContentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Image.asset(
+          'assets/images/pearl_50.png',
+          color: Colors.white,
+          scale: 1.5,
+        ),
+        title: const Text(
+          'Жемчужины Тарифи',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.brown[800],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepPurple,
+        splashColor: Colors.purpleAccent,
+        mini: true,
+        child: Center(
+          child: Icon(
+            CupertinoIcons.arrow_2_squarepath,
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {
+          // Вызвать контроллер для перехода
+        },
+      ),
+      body: CupertinoScrollbar(
+        child: ContentList(),
+      ),
+    );
+  }
+}
