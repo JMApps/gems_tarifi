5444c6f14c59ba0af8995d2bf5cd7b41d657b3b1
jmapps <jmappsmuslim@gmail.com>
2022-07-21 12:43:55 +0300

    Added hive libraries for setting's app

---
+++ lib/domain/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const String keyMainSettings = 'key_main_settings';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gems_tarifi/domain/constants.dart';
 import 'package:gems_tarifi/domain/states/provider/main_app_state.dart';
 import 'package:gems_tarifi/presentation/pages/main_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  await Hive.initFlutter();
+  await Hive.openBox(Constants.keyMainSettings);
   runApp(
     MultiProvider(
       providers: [
