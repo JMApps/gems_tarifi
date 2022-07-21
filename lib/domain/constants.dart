9fc2643e1c4a96474c550a73601f1296a466ad5e
jmapps <jmappsmuslim@gmail.com>
2022-07-21 15:06:57 +0300

    Added key's

--- lib/domain/constants.dart
+++ lib/domain/constants.dart
@@ -1,3 +1,7 @@
 class Constants {
   static const String keyMainSettings = 'key_main_settings';
+  static const String keyThemeMode = 'key_theme_mode';
+  static const String keyContentTextSize = 'key_content_text_size';
+  static const String keyContentTextColor = 'key_content_text_color';
+  static const String keyContentTextAlignIndex = 'key_content_text_align_index';
 }
