51af43720fb1068684967ea5af65ba21dbbfc86f
jmapps <jmappsmuslim@gmail.com>
2022-07-21 11:39:28 +0300

    Updated favorite page with list

---
+++ lib/presentation/pages/favorite_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gems_tarifi/presentation/widgets/favorite_list.dart';
+
+class FavoritePage extends StatelessWidget {
+  FavoritePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        leading: Image.asset(
+          'assets/images/pearl_50.png',
+          color: Colors.white,
+          scale: 1.5,
+        ),
+        title: const Text(
+          'Избранное',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.brown[800],
+      ),
+      body: CupertinoScrollbar(
+        child: FavoriteList(),
+      ),
+    );
+  }
+}
--- lib/presentation/widgets/favorite_list.dart
+++ lib/presentation/widgets/favorite_list.dart
@@ -1,31 +1,25 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gems_tarifi/data/database_query.dart';
-import 'package:gems_tarifi/provider/item_bookmark_state.dart';
-import 'package:gems_tarifi/widgets/content_item.dart';
+import 'package:gems_tarifi/presentation/widgets/content_item.dart';
+import 'package:gems_tarifi/provider/main_app_state.dart';
 import 'package:provider/provider.dart';
 
 class FavoriteList extends StatelessWidget {
   FavoriteList({Key? key}) : super(key: key);
 
-  final _databaseQuery = DatabaseQuery();
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List>(
-      future: context.watch<ItemBookmarkState>().getBookmarkState ?
-      _databaseQuery.getFavoriteContent() : _databaseQuery.getFavoriteContent(),
+      future:
+          context.watch<MainAppState>().getDatabaseQuery.getFavoriteContent(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         return snapshot.hasData
-            ? Scrollbar(
-                child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (BuildContext context, int index) {
-                      return ContentItem(item: snapshot.data![index]);
-                  },
-                ),
-              )
+            ? ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ContentItem(item: snapshot.data![index]);
+              },
+            )
             : Center(
                 child: Padding(
                   padding: const EdgeInsets.all(16),
@@ -34,13 +28,16 @@ class FavoriteList extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Icon(
-                        Icons.bookmark_border,
+                        Icons.bookmark_rounded,
                         size: 150,
                         color: Colors.brown[100],
                       ),
                       const Text(
                         'Список избранного пуст',
-                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black45,
+                        ),
                       ),
                     ],
                   ),
