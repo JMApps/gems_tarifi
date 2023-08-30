142f17a033fb1c9ea04288846e15d5ba6d9a373b
dev_muslim <jmappsmuslim@gmail.com>
2023-08-30 06:28:37 +0300

    Clear all project

---
+++ android/app/src/main/kotlin/com/example/gems_tarifi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gems_tarifi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:gems_tarifi/presentation/pages/root_page.dart';
+
+void main() {
+  runApp(
+    const RootPage(),
+  );
+}
+
+
