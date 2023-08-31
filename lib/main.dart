8f0f10322abda1967619dd53aed831c6f5ae50b9
dev_muslim <jmappsmuslim@gmail.com>
2023-08-31 23:09:48 +0300

    Fixes adaptive and user theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gems_tarifi/application/state/content_settings_state.dart';
+import 'package:gems_tarifi/application/state/main_app_state.dart';
 import 'package:gems_tarifi/application/strings/app_constraints.dart';
 import 'package:gems_tarifi/presentation/pages/root_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +23,16 @@ void main() async {
   await Hive.openBox(AppConstraints.keyAppSettingsBox);
   await Hive.openBox(AppConstraints.keyAppFavoritesBox);
   runApp(
-    const RootPage(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MainAppState(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ContentSettingsState(),
+        ),
+      ],
+      child: const RootPage(),
+    ),
   );
 }
--- lib/presentation/pages/root_page.dart
+++ lib/presentation/pages/root_page.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:gems_tarifi/application/state/content_settings_state.dart';
-import 'package:gems_tarifi/application/state/main_app_state.dart';
 import 'package:gems_tarifi/application/strings/app_strings.dart';
 import 'package:gems_tarifi/application/themes/app_theme.dart';
 import 'package:gems_tarifi/presentation/pages/main_page.dart';
@@ -14,37 +13,33 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => MainAppState(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => ContentSettingsState(),
-        ),
-      ],
-      child: MaterialApp(
-        scrollBehavior: const MaterialScrollBehavior().copyWith(
-          dragDevices: {
-            PointerDeviceKind.mouse,
-            PointerDeviceKind.touch,
-            PointerDeviceKind.stylus,
-            PointerDeviceKind.trackpad,
-            PointerDeviceKind.unknown,
-          },
-        ),
-        builder: (context, child) {
-          return ScrollConfiguration(
-            behavior: DefaultScrollBehavior(),
-            child: child!,
-          );
+    final ContentSettingsState settings = Provider.of<ContentSettingsState>(context);
+    return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.trackpad,
+          PointerDeviceKind.unknown,
         },
-        debugShowCheckedModeBanner: false,
-        title: AppStrings.appName,
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        home: const MainPage(),
       ),
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: DefaultScrollBehavior(),
+          child: child!,
+        );
+      },
+      debugShowCheckedModeBanner: false,
+      title: AppStrings.appName,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: settings.getAdaptiveTheme
+          ? ThemeMode.system
+          : settings.getDarkTheme
+              ? ThemeMode.dark
+              : ThemeMode.light,
+      home: const MainPage(),
     );
   }
 }
